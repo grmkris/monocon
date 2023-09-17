d888e873e744ca7ef70825933ec5a40ed43d67b2
grmkris <kristjan.grm1@gmail.com>
2023-09-17 13:58:22 +0200

    inital

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+Q
+!node_modules/bun-types/types.d.ts,0/7/076d3b31bd2e62982d4862960b8fb8284960380d
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+?
+.prettierrc.cjs,2/4/24f39b4502c9804e25b84f5b72686f997cc7dfa3
+8
+index.ts,a/1/a123abce010ea7d96fa76c6a9040950b5dac59bc
+=
+tsconfig.json,6/1/61ebb9fd6e8cf9082658121d5d81e297791dacd0
---
+++ .prettierrc.cjs
@@ -0,0 +1,28 @@
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
+/** @typedef  {import("prettier").Config} PrettierConfig*/
+/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  // pluginSearchDirs: false,
+  importOrder: [
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "",
+    "^~/utils/(.*)$",
+    "^~/components/(.*)$",
+    "^~/styles/(.*)$",
+    "^~/(.*)$",
+    "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+};
+
+module.exports = config;
